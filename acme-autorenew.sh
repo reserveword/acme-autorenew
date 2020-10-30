554eac8ce5b27139685cc244e870cfe7306b0ad0
reserveword <reserveword@hit.edu.cn>
2020-10-30 21:59:34 +0800

    fixes

--- acme-autorenew.sh
+++ acme-autorenew.sh
@@ -1,11 +1,7 @@
 $ACME_DATA=$PWD/.acme.sh
 export LE_WORKING_DIR=$ACME_DATA
 
-if [ -z "$SECRET_FILES" ]; then
-    source $SECRET_FILES;
-else
-    echo "NO SECRET_FILES FOUND, EXITING"
-    exit 1
-fi
+EXEC=${SECRET_FILES:-"echo 'NO SECRET_FILES FOUND, EXITING'; exit 1"}
+$($EXEC) || exit 1
 
 acme.sh/acme.sh --renew-all
