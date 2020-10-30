4d4062d1dba1ecfb32e641de2c9d276988201516
reserveword <reserveword@hit.edu.cn>
2020-10-31 02:18:00 +0800

    some more fix

--- enable-actions.sh
+++ enable-actions.sh
@@ -3,4 +3,4 @@
 USERNAME=reserveword
 REPONAME=acme-autorenew
 
-curl https://api.github.com/repos/${USERNAME}/${REPONAME}/actions/workflows/3173821/enable -X PUT -K github-token.key
+curl https://api.github.com/repos/${USERNAME}/${REPONAME}/actions/workflows/run.yml/enable -X PUT -K github-token.key
