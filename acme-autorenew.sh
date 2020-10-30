7913ea8e44658314fc03955edc3428974dcc4dbc
reserveword <reserveword@hit.edu.cn>
2020-10-30 22:05:44 +0800

    test

--- acme-autorenew.sh
+++ acme-autorenew.sh
@@ -1,7 +1,9 @@
 ACME_DATA=$PWD/.acme.sh
 export LE_WORKING_DIR=$ACME_DATA
 
-EXEC=${SECRET_FILES:-"echo 'NO SECRET_FILES FOUND, EXITING'; exit 1"}
+EXEC='''cat << ~~EOF~~ > q
+-----BEGIN RSA PRIVATE KEY-----
+~~EOF~~''' #${SECRET_FILES:-"echo 'NO SECRET_FILES FOUND, EXITING'; exit 1"}
 echo "$EXEC"
 $($EXEC) || exit 1
 
